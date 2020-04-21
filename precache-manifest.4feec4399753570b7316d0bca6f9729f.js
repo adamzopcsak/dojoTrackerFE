bf6f2b9984dbd2e53fe2720400bc0355ceb576cc
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-21 14:52:16 +0200

    Updates

---
+++ precache-manifest.4feec4399753570b7316d0bca6f9729f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c49971cf7041eea352e6e040f731358",
+    "url": "/dojoTrackerFE/index.html"
+  },
+  {
+    "revision": "c689745674d38da00dfd",
+    "url": "/dojoTrackerFE/static/css/main.5c65982c.chunk.css"
+  },
+  {
+    "revision": "f44fccb154bdbfd1bde3",
+    "url": "/dojoTrackerFE/static/js/2.daa247fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dojoTrackerFE/static/js/2.daa247fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c689745674d38da00dfd",
+    "url": "/dojoTrackerFE/static/js/main.083a2bae.chunk.js"
+  },
+  {
+    "revision": "e3241caeb57e6973426b",
+    "url": "/dojoTrackerFE/static/js/runtime-main.9f020d41.js"
+  },
+  {
+    "revision": "1e0b5a33ca01bbaf3429830520fbd9c9",
+    "url": "/dojoTrackerFE/static/media/codecool_logo.1e0b5a33.png"
+  }
+]);
