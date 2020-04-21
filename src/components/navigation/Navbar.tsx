a39403c07888174c6b2c635b10f8fcbf695c6203
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-21 11:13:50 +0200

    Changed content style and added links

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import HomeLink from "./HomeLink";
 import SignIn from "../user-management/SignIn";
+import NavLinks from "./NavLinks";
 
 const StyledNavBar = styled.nav`
     width: 80%;
     max-width: 100%;
     height: 8vh;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: center;
     z-index: 1;
     color: black;
@@ -20,6 +21,7 @@ const Navbar = (props: Props) => {
     return (
         <StyledNavBar>
             <HomeLink />
+            <NavLinks />
             <SignIn />
         </StyledNavBar>
     );
