61ac4972e6502e22d8ccd5f72201939c4f07b488
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-21 17:09:10 +0200

    implemented check for completion status

--- src/components/dojos/DojoList.tsx
+++ src/components/dojos/DojoList.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Container } from "../styled-components/Reusables";
 import { IBasicDojoInfo } from "../../static/util/interfaces";
 import DojoBasic from "./DojoBasic";
 import styled from "styled-components";
+import { UserContext } from "../context/UserContextProvider";
 
 const ContainerWithRows = styled(Container)`
     flex-direction: row;
@@ -13,16 +14,21 @@ interface Props {}
 
 const DojoList = (props: Props) => {
     const [dojos, setDojos] = useState<null | IBasicDojoInfo[]>();
+    const [user, setUser] = useContext(UserContext);
 
     useEffect(() => {
         setDojos(require("../../static/test data/dojos.json"));
     }, []);
 
+    const isDojoComplete = (id: number): boolean => {
+        return user.completedDojos.includes(id);
+    };
+
     return (
         <ContainerWithRows>
             {dojos &&
                 dojos.map((dojo) => {
-                    return <DojoBasic key={dojo.id} dojo={dojo} />;
+                    return <DojoBasic key={dojo.id} dojo={dojo} isComplete={isDojoComplete(dojo.id)} />;
                 })}
         </ContainerWithRows>
     );
