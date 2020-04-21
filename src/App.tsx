3038f1579ef7a77328e2a49e74d05fcc8786c35a
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-21 14:44:04 +0200

    Added route for dojo list

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/navigation/Navbar";
+import DojoList from "./components/dojos/DojoList";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Navbar />
             <Switch>
                 <Route exact path="/" component={LandingPage} />
+                <Route exact path="/dojos" component={DojoList} />
             </Switch>
         </Router>
     );
