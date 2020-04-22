152e3690e3a6d47fa3dfd36f7bd0a4f64afee8c5
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-22 17:19:58 +0200

    Added context to keep track of solution programming lang

---
+++ src/components/context/SolutionLanguageProvider.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+export const SolutionLanguageContext = createContext();
+
+const SolutionLanguageProvider = (props) => {
+    const [language, setLanguage] = useState("python");
+
+    return (
+        <SolutionLanguageContext.Provider value={[language, setLanguage]}>
+            {props.children}
+        </SolutionLanguageContext.Provider>
+    );
+};
+
+export default SolutionLanguageProvider;
