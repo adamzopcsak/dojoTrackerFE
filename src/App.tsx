f6189e186d7f48c08252bbf0d8eba81c21c8eb6f
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-22 17:18:59 +0200

    Added multiple context providers

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,27 @@ import LandingPage from "./components/LandingPage";
 import Navbar from "./components/navigation/Navbar";
 import DojoList from "./components/dojos/DojoList";
 import UserContextProvider from "./components/context/UserContextProvider";
+import DojoContextProvider from "./components/context/DojoContextProvider";
+import DojoDetailed from "./components/dojos/DojoDetailed";
+import SolutionLanguageProvider from "./components/context/SolutionLanguageProvider";
+import EditorThemeProvider from "./components/context/EditorThemeProvider";
 
 function App() {
     return (
         <Router>
             <UserContextProvider>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={LandingPage} />
-                    <Route exact path="/dojos" component={DojoList} />
-                </Switch>
+                <DojoContextProvider>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={LandingPage} />
+                        <Route exact path="/dojos" component={DojoList} />
+                        <SolutionLanguageProvider>
+                            <EditorThemeProvider>
+                                <Route path="/dojos/:id" component={DojoDetailed} />
+                            </EditorThemeProvider>
+                        </SolutionLanguageProvider>
+                    </Switch>
+                </DojoContextProvider>
             </UserContextProvider>
         </Router>
     );
