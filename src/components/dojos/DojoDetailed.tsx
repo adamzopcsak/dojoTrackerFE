2fc97fe35934abe7bbc544964ddb3e203cd16b38
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-22 17:20:31 +0200

    Added indivcidual dojo page with editor

---
+++ src/components/dojos/DojoDetailed.tsx
@@ -0,0 +1,41 @@
+import React, { useContext, useState, useEffect, Fragment } from "react";
+import { ContainerWithRows } from "../styled-components/Reusables";
+import { useParams } from "react-router-dom";
+import { DojoContext } from "../context/DojoContextProvider";
+import { IBasicDojoInfo } from "../../static/util/interfaces";
+import SolutionEditor from "./SolutionEditor";
+import DojoInfo from "./DojoInfo";
+
+interface Props {}
+
+const DojoDetailed = (props: Props) => {
+    const { id } = useParams();
+
+    const [dojos, setDojos] = useContext(DojoContext);
+
+    const [dojo, setDojo] = useState<null | IBasicDojoInfo>();
+
+    useEffect(() => {
+        const dojo = getDojo();
+
+        setDojo(dojo);
+    }, [id]);
+
+    const getDojo = (): IBasicDojoInfo => {
+        return dojos.find((dojo: IBasicDojoInfo) => dojo.id.toString() === id);
+    };
+
+    return (
+        <ContainerWithRows>
+            {dojo && (
+                <Fragment>
+                    {""}
+                    <DojoInfo title={dojo.title} link={dojo.url} />
+                    <SolutionEditor dojoId={dojo.id} />
+                </Fragment>
+            )}
+        </ContainerWithRows>
+    );
+};
+
+export default DojoDetailed;
