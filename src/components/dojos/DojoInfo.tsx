bcbc817c9443eeedd5ff2971dd20f34be2fe94d9
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-23 18:49:28 +0200

    Added status indicator change event (only logs atm)

--- src/components/dojos/DojoInfo.tsx
+++ src/components/dojos/DojoInfo.tsx
@@ -1,19 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 import { EmptyButton } from "../styled-components/Reusables";
+import StatusIndicator from "./StatusIndicator";
+import axios from "axios";
 
 const StyledWrapper = styled.div`
-    width: 40%;
-    height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
+    width: 25%;
+    height: auto;
+    font-size: 0.9rem;
+    padding: 2rem;
+    margin: 0.7rem;
+    text-align: center;
+
+    @media screen and (max-width: 768px) {
+        width: 80%;
+    }
+
+    & div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        text-align: center;
+    }
+
+    & p,
+    button {
+        margin: 2rem;
+        font-size: 1.2rem;
+    }
 `;
 
 interface Props {
     title: string;
     link: string;
+    isComplete: boolean;
 }
 
 const DojoInfo = (props: Props) => {
@@ -21,9 +46,16 @@ const DojoInfo = (props: Props) => {
         window.open(props.link, "_blank");
     };
 
+    const changeStatus = (status: any) => {};
+
     return (
         <StyledWrapper>
             <h1>{props.title}</h1>
+            <div>
+                <p>Mark as complete:</p>
+                <StatusIndicator isComplete={props.isComplete} onStatusChange={changeStatus} />
+            </div>
+
             <EmptyButton onClick={goToDojoPage}>Attempt</EmptyButton>
         </StyledWrapper>
     );
