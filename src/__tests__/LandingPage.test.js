0f524f005d9941500ea2c185be4cfe3df0ae380d
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-04-27 17:07:51 +0200

    Removed navbar specific info

--- src/__tests__/LandingPage.test.js
+++ src/__tests__/LandingPage.test.js
@@ -1,34 +1,12 @@
 import React from "react";
-import { render, getByText } from "@testing-library/react";
-import LandingPage from "../components/LandingPage";
+import { render, cleanup } from "@testing-library/react";
 import App from "../App";
+
+afterEach(cleanup);
 
 test("Renders without crashing", () => {
     const { getByText } = render(<App />);
     const landingPageTitle = getByText("ASD");
 
     expect(landingPageTitle).toBeInTheDocument();
-});
-
-describe("While not logged in", () => {
-    test("Disabled menus don't render", () => {
-        const { queryByText } = render(<App />);
-        const dojoMenu = queryByText("Dojos");
-
-        expect(dojoMenu).not.toBeInTheDocument();
-    });
-
-    test("Sign-in button renders correctly", () => {
-        const { queryByText } = render(<App />);
-        const signInBtn = queryByText("Sign in");
-
-        expect(signInBtn).toBeInTheDocument();
-    });
-
-    test("Log-out button doesn't render", () => {
-        const { queryByText } = render(<App />);
-        const logOutBtn = queryByText("Log out");
-
-        expect(logOutBtn).not.toBeInTheDocument();
-    });
 });
