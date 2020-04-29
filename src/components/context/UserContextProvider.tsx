0ca0202e91a16b21f6e820b8bd3c4dc974583d18
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-29 11:07:11 +0200

    User Context provider is now a typescript file

---
+++ src/components/context/UserContextProvider.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useEffect, ReactNode } from "react";
+import { loadState, saveState } from "../../static/util/persist";
+
+interface ContextStateProp {
+    user: any;
+    setUser: Function;
+}
+
+export const UserContext = createContext<ContextStateProp>({} as ContextStateProp);
+
+const UserContextProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState("");
+
+    useEffect(() => {
+        const persistedState = loadState();
+        if (persistedState) {
+            setUser(persistedState);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (user) {
+            saveState(user);
+        }
+    }, [user]);
+
+    return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export default UserContextProvider;
