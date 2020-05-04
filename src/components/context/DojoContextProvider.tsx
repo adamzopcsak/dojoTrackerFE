0478b6570a7b3e3661a011685df14603ae88cf1a
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-04 12:14:34 +0200

    Added api call to context

--- src/components/context/DojoContextProvider.tsx
+++ src/components/context/DojoContextProvider.tsx
@@ -1,5 +1,8 @@
-import React, { createContext, useState, ReactNode } from "react";
+import React, { createContext, useState, ReactNode, useContext, useEffect } from "react";
 import { IBasicDojoInfo } from "../../static/util/interfaces";
+import { AxiosResponse } from "axios";
+import { UserContext } from "./UserContextProvider";
+import axios from "axios";
 
 interface ContextStateProp {
     dojos: any | IBasicDojoInfo;
@@ -9,7 +12,17 @@ interface ContextStateProp {
 export const DojoContext = createContext<ContextStateProp>({} as ContextStateProp);
 
 const DojoContextProvider = ({ children }: { children: ReactNode }) => {
-    const [dojos, setDojos] = useState();
+    const [dojos, setDojos] = useState<any | IBasicDojoInfo>();
+
+    const { user } = useContext(UserContext);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5000/api/dojo/list?id=${user.id}`)
+            .then((response: AxiosResponse<IBasicDojoInfo>) => {
+                setDojos(response.data);
+            });
+    }, [user]);
 
     return <DojoContext.Provider value={{ dojos, setDojos }}>{children}</DojoContext.Provider>;
 };
