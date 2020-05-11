16677e26b3b7990d991a7cd263d99962bb2b65e6
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-11 18:05:48 +0200

    Added mark as complete method

--- src/components/solution/Solution.tsx
+++ src/components/solution/Solution.tsx
@@ -4,16 +4,24 @@ import { IBasicDojoInfo } from "../../static/util/interfaces";
 import DojoInfo from "./DojoInfo";
 import SolutionEditor from "./SolutionEditor";
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const CustomContainer = styled(ContainerWithRows)`
     margin-top: 1% !important;
 `;
 
 const Solution = ({ dojo }: { dojo: IBasicDojoInfo }) => {
+    const history = useHistory();
+
+    const markAsComplte = () => {
+        dojo.isDone = true;
+        history.push(`/dojos/${dojo.id}/sucess`);
+    };
+
     return (
         <CustomContainer>
             <DojoInfo title={dojo.title} link={dojo.url} isComplete={dojo.isDone} />
-            <SolutionEditor isComplete={dojo.isDone} />
+            <SolutionEditor isComplete={dojo.isDone} markAsComplete={markAsComplte} />
         </CustomContainer>
     );
 };
