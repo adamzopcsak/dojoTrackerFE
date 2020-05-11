d3109d766029251e3e530fc04e9b49c46df518eb
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-11 12:35:33 +0200

    Style changes

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -7,21 +7,22 @@ import NavLinks from "./NavLinks";
 import { LoginContext } from "../context/LoginContextProvider";
 
 const StyledNavBar = styled.nav`
-    width: 90%;
+    width: 100%;
     max-width: 100%;
     height: 8vh;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
     align-items: center;
     z-index: 1;
     color: black;
     font-weight: bold;
-    position: fixed;
+    position: sticky;
+    top: 0;
     background-color: rgba(255, 255, 255, 0.8);
     top: 0;
 
     & * {
-        margin: 0 0.5rem;
+        /* margin: 0 0.5rem; */
     }
 `;
 
