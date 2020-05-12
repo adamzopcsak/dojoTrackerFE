99885ee05f389685ba18c71db592b94213832ad1
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-12 16:43:41 +0200

    Added a checker to context to directly check status from localstorage

--- src/components/context/LoginContextProvider.tsx
+++ src/components/context/LoginContextProvider.tsx
@@ -2,14 +2,15 @@ import React, { ReactNode, createContext, useState, useEffect } from "react";
 import { loadState, saveState } from "../../static/util/persist";
 
 interface ContextStateProp {
-    isLoggedIn: boolean;
+    isLoggedIn: any;
     setIsLoggedIn: Function;
+    checkForLoginStatus: Function;
 }
 
 export const LoginContext = createContext({} as ContextStateProp);
 
 const LoginContextProvider = ({ children }: { children: ReactNode }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState();
 
     useEffect(() => {
         const persistedState = loadState();
@@ -24,7 +25,21 @@ const LoginContextProvider = ({ children }: { children: ReactNode }) => {
         }
     }, [isLoggedIn]);
 
-    return <LoginContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>{children}</LoginContext.Provider>;
+    const checkForLoginStatus = (): boolean => {
+        try {
+            const serializedState = localStorage.getItem("dta-login-state");
+            if (serializedState === null) return false;
+            return JSON.parse(serializedState).isLoggedIn;
+        } catch (err) {
+            return false;
+        }
+    };
+
+    return (
+        <LoginContext.Provider value={{ isLoggedIn, setIsLoggedIn, checkForLoginStatus }}>
+            {children}
+        </LoginContext.Provider>
+    );
 };
 
 export default LoginContextProvider;
