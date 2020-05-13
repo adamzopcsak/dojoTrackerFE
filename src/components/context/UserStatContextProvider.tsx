423e46f3799cd791eaa90e43da03904c6ac7bf4d
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-13 15:19:02 +0200

    Removed console log and useEffect dep array

--- src/components/context/UserStatContextProvider.tsx
+++ src/components/context/UserStatContextProvider.tsx
@@ -5,21 +5,23 @@ import { AxiosResponse } from "axios";
 
 interface ContextStateProp {
     userStats: any | IUserStatistics;
+    isHidden: boolean;
+    setIsHidden: Function;
 }
 
 export const UserStatContext = createContext({} as ContextStateProp);
 
 const UserStatContextProvider = ({ children }: { children: ReactNode }) => {
     const [userStats, setUserStats] = useState<any | IUserStatistics[]>();
+    const [isHidden, setIsHidden] = useState<boolean>(true);
 
     useEffect(() => {
-        axios.get("/api/stats/users").then((response: AxiosResponse<IUserStatistics>) => {
+        axios.get("/api/stats/users").then((response: AxiosResponse<IUserStatistics[]>) => {
             setUserStats(response.data);
-            console.log("this is being fetched");
         });
-    }, [setUserStats, userStats]);
+    }, []);
 
-    return <UserStatContext.Provider value={{ userStats }}></UserStatContext.Provider>;
+    return <UserStatContext.Provider value={{ userStats, isHidden, setIsHidden }}>{children}</UserStatContext.Provider>;
 };
 
 export default UserStatContextProvider;
