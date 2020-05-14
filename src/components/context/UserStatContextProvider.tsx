cc69154969da7a562e8510f3c43b7ae54ca5d0f8
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-14 16:06:56 +0200

    Added sort and date normalization to the useeffect hook

--- src/components/context/UserStatContextProvider.tsx
+++ src/components/context/UserStatContextProvider.tsx
@@ -2,11 +2,13 @@ import React, { createContext, ReactNode, useState, useEffect } from "react";
 import { IUserStatistics } from "../../static/util/interfaces";
 import axios from "../../static/util/axiosConfig";
 import { AxiosResponse } from "axios";
+import { sortAscByValue, sortDescByValue } from "../../static/util/sort";
 
 interface ContextStateProp {
-    userStats: any | IUserStatistics;
+    userStats: any | IUserStatistics[];
     isHidden: boolean;
     setIsHidden: Function;
+    sortData: Function;
 }
 
 export const UserStatContext = createContext({} as ContextStateProp);
@@ -14,14 +16,35 @@ export const UserStatContext = createContext({} as ContextStateProp);
 const UserStatContextProvider = ({ children }: { children: ReactNode }) => {
     const [userStats, setUserStats] = useState<any | IUserStatistics[]>();
     const [isHidden, setIsHidden] = useState<boolean>(true);
+    const [isDesc, setIsDesc] = useState(true);
 
     useEffect(() => {
         axios.get("/api/stats/users").then((response: AxiosResponse<IUserStatistics[]>) => {
-            setUserStats(response.data);
+            response.data.forEach((stat) => {
+                stat.lastCompleted = normalizeDate(stat.lastCompleted);
+            });
+
+            setUserStats(response.data.sort(sortDescByValue("name")));
         });
     }, []);
 
-    return <UserStatContext.Provider value={{ userStats, isHidden, setIsHidden }}>{children}</UserStatContext.Provider>;
+    const sortData = (value: string) => {
+        isDesc
+            ? setUserStats(userStats.sort(sortAscByValue(value)))
+            : setUserStats(userStats.sort(sortDescByValue(value)));
+
+        setIsDesc(!isDesc);
+    };
+
+    const normalizeDate = (date: string): Date => {
+        return new Date(Date.parse(date));
+    };
+
+    return (
+        <UserStatContext.Provider value={{ userStats, isHidden, setIsHidden, sortData }}>
+            {children}
+        </UserStatContext.Provider>
+    );
 };
 
 export default UserStatContextProvider;
