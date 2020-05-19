f31823a77ec1d093d9f2e4545a46ded74a8d363f
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-05-19 18:04:20 +0200

    Moved styling to reusables

--- src/components/error/NoAccess.tsx
+++ src/components/error/NoAccess.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { EmptyButton, Container } from "../styled-components/Reusables";
-import styled from "styled-components";
+import { EmptyButton, CenteredContainer } from "../styled-components/Reusables";
 
-const CustomContainer = styled(Container)`
-    & button {
-        margin: 1rem 2rem;
-    }
-`;
 const NoAccess = () => {
     const history = useHistory();
 
@@ -16,10 +10,10 @@ const NoAccess = () => {
     };
 
     return (
-        <CustomContainer>
+        <CenteredContainer>
             <h3>You have no access to view this page</h3>
             <EmptyButton onClick={goBack}>Back</EmptyButton>
-        </CustomContainer>
+        </CenteredContainer>
     );
 };
 
