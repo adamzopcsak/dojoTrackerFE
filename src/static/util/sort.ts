5d594e67dbbbd0e4d02d57a18651df5c23bc3eac
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-20 13:20:01 +0200

    Changed sort function names to reflext what they actually do.

--- src/static/util/sort.ts
+++ src/static/util/sort.ts
@@ -1,5 +1,5 @@
-export const sortDescByValue = (value: any) => (a: any, b: any) =>
+export const sortAscByValue = (value: any) => (a: any, b: any) =>
     a[value] === b[value] ? 0 : a[value] < b[value] ? -1 : 1;
 
-export const sortAscByValue = (value: any) => (a: any, b: any) =>
+export const sortDescByValue = (value: any) => (a: any, b: any) =>
     a[value] === b[value] ? 0 : a[value] < b[value] ? 1 : -1;
