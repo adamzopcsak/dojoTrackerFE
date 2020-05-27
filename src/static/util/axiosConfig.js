419a17537494368ed908cd77ae01852627ca4448
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-05-27 12:22:13 +0200

    Added new environment to config

--- src/static/util/axiosConfig.js
+++ src/static/util/axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import customHistory from "./customHistory";
+import runtimeEnv from "@mars/heroku-js-runtime-env";
 
-const API_URL = process.env.API_URL;
+const env = runtimeEnv();
+
+const API_URL = env.REACT_APP_API_URL;
 const TIMEOUT = 5000;
 
 const instance = axios.create({
