a3dd64109f10f4ceb4b1af0f1bd769d5930e8069
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-06-22 18:25:39 +0200

    added dev url back

--- src/static/util/axiosConfig.js
+++ src/static/util/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import customHistory from "./customHistory";
 
-const API_URL = "https://track-that-dojo.herokuapp.com";
+const API_URL = "http://localhost:5000";
 const TIMEOUT = 5000;
 
 const instance = axios.create({
