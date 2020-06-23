786a8337fb49ced26204fb0736a90ead0995c69a
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-06-23 14:26:27 +0200

    Implemented onclick input and save

--- src/components/user-profile/settings/ProfileData.tsx
+++ src/components/user-profile/settings/ProfileData.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext, KeyboardEvent } from "react";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
+import { ProfilePageContext } from "../../context/ProfilePageContextProvider";
+import { UserDataContext } from "../../context/UserDataContextProvider";
 
 const StyledPropfileData = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: flex-start;
+    justify-content: left;
+    align-items: center;
+    text-align: left !important;
     flex-direction: column;
-    margin: 2rem 5rem;
-    width: 30%;
+
+    width: 100%;
 
     .profile-data-title {
         font-weight: bold;
@@ -24,6 +27,28 @@ const StyledPropfileData = styled.div`
 
     svg {
         color: #dc3545;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+    .profile-data-input {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        position: relative;
+
+        input {
+            border: none;
+            background-color: inherit;
+            font-weight: bold;
+            color: gray;
+
+            &:focus {
+                outline: none;
+            }
+        }
     }
 `;
 
@@ -35,8 +60,30 @@ interface Props {
 const ProfileData = (props: Props) => {
     const [isEdited, setIsEdited] = useState(false);
 
+    const [valueToEdit, setValueToEdit] = useState(props.data);
+
+    const { user, setUser } = useContext(UserDataContext);
+
+    const changeNickname = (value: string) => {
+        let changedUserData = user;
+
+        changedUserData.nickName = value;
+
+        setValueToEdit(value);
+
+        setUser(changedUserData);
+    };
+
+    const stopEditingOnKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            setIsEdited(!isEdited);
+        }
+
+        return;
+    };
+
     return (
-        <div className="profile-data-div">
+        <StyledPropfileData className="profile-data-div">
             <p className="profile-data-title"> Nickname:</p>
             <div
                 className="data-edit-cluster"
@@ -45,21 +92,31 @@ const ProfileData = (props: Props) => {
                 }}
             >
                 {isEdited ? (
-                    <input
-                        value={props.data}
-                        onChange={(event: any) => {
-                            props.data = event.target.value;
-                        }}
-                    ></input>
+                    <div className="profile-data-input">
+                        <input
+                            autoFocus
+                            value={valueToEdit}
+                            onChange={(event: any) => {
+                                changeNickname(event.target.value);
+                            }}
+                            onKeyDown={(event) => {
+                                stopEditingOnKeyDown(event);
+                            }}
+                        ></input>
+                    </div>
                 ) : (
-                    <p>{props.data !== null ? props.data : "default"}</p>
+                    <p>{valueToEdit !== null ? valueToEdit : "default"}</p>
                 )}
 
-                <p>
+                <p
+                    onClick={() => {
+                        setIsEdited(!isEdited);
+                    }}
+                >
                     <FontAwesomeIcon icon={faPen} onClick={() => {}} />
                 </p>
             </div>
-        </div>
+        </StyledPropfileData>
     );
 };
 
