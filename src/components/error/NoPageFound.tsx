c21f7e4cf677bbbf4f8da40089d53b3e78f7ab5d
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-06-23 14:25:20 +0200

    Changed goback to go to main site

--- src/components/error/NoPageFound.tsx
+++ src/components/error/NoPageFound.tsx
@@ -6,14 +6,16 @@ const NoPageFound = () => {
     const history = useHistory();
 
     const goBack = () => {
-        history.goBack();
+        history.push("/");
     };
 
     return (
         <CenteredContainer>
             <h1>Error: 404</h1>
             <h3>The page you are looking for does not exist or have been removed.</h3>
-            <EmptyButton onClick={goBack} id={"goback-btn"}>Back</EmptyButton>
+            <EmptyButton onClick={goBack} id={"goback-btn"}>
+                Back
+            </EmptyButton>
         </CenteredContainer>
     );
 };
