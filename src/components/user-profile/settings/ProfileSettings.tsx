5bc5bb040ca109542aa0967786ecce4da808c199
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-06-23 14:26:46 +0200

    Removed unused CSS added styling to select and added save methods

--- src/components/user-profile/settings/ProfileSettings.tsx
+++ src/components/user-profile/settings/ProfileSettings.tsx
@@ -3,96 +3,87 @@ import { ProfilePageContext } from "../../context/ProfilePageContextProvider";
 import styled from "styled-components";
 import { StyledProfileContainer, EmptyButton } from "../../styled-components/Reusables";
 import ProfileData from "./ProfileData";
+import { useHistory } from "react-router-dom";
+import { UserDataContext } from "../../context/UserDataContextProvider";
 
-const StyledProfileSettings = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+const StyledProfileSettings = styled(StyledProfileContainer)`
+    justify-content: flex-start;
     align-items: center;
     flex-wrap: wrap;
     color: gray;
 
-    .profile-data-div {
-        display: flex;
-        justify-content: center;
-        align-items: flex-start;
-        flex-direction: column;
-        margin: 2rem 5rem;
-        width: 30%;
-    }
-
-    .profile-data-title {
-        font-weight: bold;
-    }
-
-    .data-edit-cluster {
-        display: flex;
-        flex-direction: row;
-        width: 40%;
-        justify-content: space-between;
-    }
-
-    svg {
-        color: #dc3545;
+    .select {
+        margin: 2rem 0;
+        width: 50%;
     }
 `;
 
 interface Props {}
 
 const ProfileSettings = (props: Props) => {
-    const { userData } = useContext(ProfilePageContext);
+    const { user, saveUpdates } = useContext(UserDataContext);
+    const history = useHistory();
+
+    const updatePreferredLanguage = (value: string) => {
+        user.preferredLanguage = value;
+    };
+
+    const updatePreferredEditorTheme = (value: string) => {
+        user.preferredEditorTheme = value;
+    };
+
+    const saveChanges = () => {
+        saveUpdates();
+        history.push("/user-update-success");
+    };
 
     return (
-        <StyledProfileContainer>
+        <StyledProfileSettings>
             <h3>Settings</h3>
-            <StyledProfileSettings>
-                <ProfileData header="Nickname:" data={userData.nickName} />
-                <div className="select">
-                    <select
-                        className="select-text"
-                        required
-                        onChange={(event) => {}}
-                        id="issue-type-dropdown"
-                        defaultValue="  "
-                    >
-                        <option disabled value=""></option>
-                        <option value="bug">Bug</option>
-                        <option value="suggestion">Suggestion</option>
-                    </select>
-                    <label className="select-label">Select issue type</label>
-                </div>
-                <div className="select">
-                    <select
-                        className="select-text"
-                        required
-                        onChange={(event) => {}}
-                        id="issue-type-dropdown"
-                        defaultValue="  "
-                    >
-                        <option disabled value=""></option>
-                        <option value="bug">Bug</option>
-                        <option value="suggestion">Suggestion</option>
-                    </select>
-                    <label className="select-label">Select issue type</label>
-                </div>
 
-                <div className="select">
-                    <select
-                        className="select-text"
-                        required
-                        onChange={(event) => {}}
-                        id="issue-type-dropdown"
-                        defaultValue=""
-                    >
-                        <option disabled value=""></option>
-                        <option value="bug">Bug</option>
-                        <option value="suggestion">Suggestion</option>
-                    </select>
-                    <label className="select-label">Select issue type</label>
-                </div>
-            </StyledProfileSettings>
-            <EmptyButton danger>Save changes</EmptyButton>
-        </StyledProfileContainer>
+            <ProfileData header="Nickname:" data={user.nickName} />
+            <div className="select">
+                <select
+                    className="select-text"
+                    required
+                    onChange={(event: any) => {
+                        updatePreferredLanguage(event.target.value);
+                    }}
+                    id="issue-type-dropdown"
+                    defaultValue={user.preferredLanguage !== null ? user.preferredLanguage : "python"}
+                >
+                    <option value="python">Python</option>
+                    <option value="csharp">C#</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="java">Java</option>
+                </select>
+                <label className="select-label">Select language preference</label>
+            </div>
+            <div className="select">
+                <select
+                    className="select-text"
+                    required
+                    onChange={(event: any) => {
+                        updatePreferredEditorTheme(event.target.value);
+                    }}
+                    id="issue-type-dropdown"
+                    defaultValue={user.preferredEditorTheme !== null ? user.preferredEditorTheme : "dark"}
+                >
+                    <option value="monokai">Dark</option>
+                    <option value="github">Light</option>
+                </select>
+                <label className="select-label">Select editor theme preference</label>
+            </div>
+
+            <div className="select">
+                <select className="select-text" required onChange={(event) => {}} id="issue-type-dropdown" disabled>
+                    <option value="">Dark</option>
+                    <option value="">Light</option>
+                </select>
+                <label className="select-label">Select website theme</label>
+            </div>
+            <EmptyButton onClick={() => saveChanges()}>Save changes</EmptyButton>
+        </StyledProfileSettings>
     );
 };
 
